27c546305c6ed21c09a5f9a9d8253f2eb5ac74df
redwanX <redwanistbd2@gmail.com>
2022-05-08 15:39:24 +0600

    fix alignments

--- src/Pages/TableData/TableDataActivity.js
+++ src/Pages/TableData/TableDataActivity.js
@@ -6,7 +6,7 @@ const TableDataActivity = (props) => {
     <tr className='secondery-bg mb-lg-0 mb-5'>
     <td className='fw-bold text-center' data-lebel="Product Name">{name}</td>
     <td className='fw-bold text-center' data-lebel="Date">{date}</td>
-    <td className='fw-bold text-center' data-lebel="Action">{action === 'delete'?<p className=' btn light-text fw-bold bg-danger'>DELETED</p>:<p className=' btn light-text fw-bold bg-success'>ADDED</p>}</td>
+    <td className='fw-bold text-center' data-lebel="Action">{action === 'delete'?<span className=' btn light-text fw-bold bg-danger'>DELETED</span>:<span className=' btn light-text fw-bold bg-success'>ADDED</span>}</td>
 
   </tr>
   )
