9e5f9e14dc1a48808f567d90dcdf0ff89dd1c4d9
redwanX <redwanistbd2@gmail.com>
2022-05-08 20:32:35 +0600

    aligning inventory

--- src/Pages/Inventory/Inventroy.js
+++ src/Pages/Inventory/Inventroy.js
@@ -58,23 +58,33 @@ const Inventroy = () => {
     setItem({...rest,quantity:newquantity})
   }
   return (
-    <div  style={{minHeight: 'calc(100vh - 116px - 74px)'}} className='container mb-3'>
+    <div  style={{minHeight: 'calc(100vh - 116px - 74px)'}} className='container my-3'>
       <div className='row row-cols-1 g-4 row-cols-lg-2'>
       <Item item={item} invetory={true}></Item>
-      <div className='my-auto'>
-        <h3 className=' mt-5 secondery-text fw-bold text-center'>RESTOCK THE ITEM</h3>
-        <Form onSubmit={updateQuantity}>
-        <Form.Group className='my-3'>
-            <Form.Control className='secondery-bg' min="0" type="number" name="quantity" placeholder="Quantity" required />
-          </Form.Group>
-        <Button className='btn btn-dark w-100 py-3' type="submit">Update Quantity</Button>
-      </Form>
+      <div className='col'>
+        <h2 className='text-center fw-bold primary-text pt-5'>UPDATE PRODUCT</h2>
+        <hr />
+            <div className="pt-5">
+              <div className='pb-5'>
+              <h3 className='secondery-text fw-bold text-center'>RESTOCK THE ITEM</h3>
+              <Form onSubmit={updateQuantity}>
+              <Form.Group className='my-3'>
+                  <Form.Control className='secondery-bg' min="0" type="number" name="quantity" placeholder="Quantity" required />
+                </Form.Group>
+              <Button className='btn btn-dark w-100 py-3' type="submit">Update Quantity</Button>
+              </Form>
+              </div>
+              <div className='pb-5'>              
+              <h3 className=' mt-5 secondery-text fw-bold text-center'>SELL THIS ITEM</h3>
+              <button className='btn btn-dark w-100 py-3' onClick={soldUpdate}>Delivered</button>
+              </div>
 
-      <h3 className=' mt-5 secondery-text fw-bold text-center'>SELL THIS ITEM</h3>
-      <button className='btn btn-dark w-100 py-3' onClick={soldUpdate}>Delivered</button>
+              <div className='pb-5'>
+              <h3 className=' mt-5 secondery-text fw-bold text-center'>MANAGE OTHER ITEMS</h3>
+              <button className='btn btn-dark w-100 py-3' onClick={()=>navigate('/manageInventory')} >Manage Inventory</button>
+              </div>
 
-      <h3 className=' mt-5 secondery-text fw-bold text-center'>MANAGE OTHER ITEMS</h3>
-      <button className='btn btn-dark w-100 py-3' onClick={()=>navigate('/manageInventory')} >Manage Inventory</button>
+            </div>
       </div>
       </div>
     </div>
